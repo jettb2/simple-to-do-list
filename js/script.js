e32d7d8400182327e0800666f93db6171ab47e08
jettb2 <jettblaschka@gmail.com>
2021-12-06 18:39:02 -0500

    update to adding new item in script,js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ function newItem() {
 //1. Adding a new item to the list of items:
 let li = $('<li></li>');
 let inputValue = $('input').val();
-let text = $('inputValue')
   li.append(inputValue);
 
 if(inputValue === '') {
